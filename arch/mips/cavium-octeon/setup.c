09748ef6450c6953f00e4f51f0145a92cd2729de
David Daney <david.daney@cavium.com>
2012-09-21 11:32:28 -0700

    MIPS: OCTEON: Print address of passed device tree.
    This allows us to verify the bootloader is doing what we ask.
    
    Signed-off-by: David Daney <david.daney@cavium.com>

--- arch/mips/cavium-octeon/setup.c
+++ arch/mips/cavium-octeon/setup.c
@@ -1239,6 +1239,7 @@ void __init device_tree_init(void)
 
 	if (octeon_bootinfo->minor_version >= 3 && octeon_bootinfo->fdt_addr) {
 		fdt = phys_to_virt(octeon_bootinfo->fdt_addr);
+		pr_info("Using passed Device Tree <%p>.\n", fdt);
 		if (fdt_check_header(fdt))
 			panic("Corrupt Device Tree passed to kernel.");
 		do_prune = false;
@@ -1255,8 +1256,6 @@ void __init device_tree_init(void)
 	if (do_prune) {
 		octeon_prune_device_tree();
 		pr_info("Using internal Device Tree.\n");
-	} else {
-		pr_info("Using passed Device Tree.\n");
 	}
 	unflatten_and_copy_device_tree();
 }
