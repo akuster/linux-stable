b3d7ba6662dfadcf25a31eab5df80f76937335c8
Leonid Rosenboim <lrosenboim@caviumnetworks.com>
2013-08-28 11:17:42 -0700

    MIPS: Octeon: Fix new coremask, hotplug CPU
    Update HOTPLUG_CPU code to handle new cvmx_coremask_t structures,
    make the allocation of global hotplug memory atomic,
    fix atomic modification of available_coremask.
    Validate hot-plugging CPUs passed between Linux and SE app.
    
    Signed-off-by: Leonid Rosenboim <lrosenboim@caviumnetworks.com>
    Signed-off-by: David Daney <david.daney@cavium.com>

--- arch/mips/cavium-octeon/setup.c
+++ arch/mips/cavium-octeon/setup.c
@@ -164,13 +164,6 @@ static int octeon_uart;
 
 extern asmlinkage void handle_int(void);
 
-/*
- * If set to a non-zero value, the bootloader entry point for
- * HOTPLUG_CPU and other tricks.
- */
-u64 octeon_bootloader_entry_addr;
-EXPORT_SYMBOL(octeon_bootloader_entry_addr);
-
 /* If an initrd named block is specified, its name goes here. */
 static char __initdata rd_name[64];
 
@@ -573,7 +566,6 @@ void __init prom_init(void)
 	struct cvmx_sysinfo *sysinfo;
 	const char *arg;
 	char *p;
-	struct linux_app_boot_info *labi;
 	int i;
 	u64 t;
 	int argc;
@@ -827,10 +819,6 @@ append_arg:
 
 	octeon_user_io_init();
 	octeon_setup_smp();
-
-	labi = phys_to_virt(LABI_ADDR_IN_BOOTLOADER);
-	if (labi->labi_signature == LABI_SIGNATURE)
-		octeon_bootloader_entry_addr = labi->InitTLBStart_addr;
 
 #ifdef CONFIG_CAVIUM_GDB
 	cvmx_debug_init ();
