5710faf2139ba07f216505c8b86b106826a7f706
Vinita Gupta <vgupta@caviumnetworks.com>
2013-11-04 11:05:58 -0800

    MIPS:OCTEON:SATA:hecked ata platform code to modify octeon SHIM according to endianess Signed-off-by: Vinita Gupta

---
+++ drivers/ata/sata_octeon.c
@@ -0,0 +1,30 @@
+/*
+ * SATA glue for Cavium Octeon III SOCs.
+ *
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2010 Cavium Networks
+ *
+ */
+
+#include <asm/octeon/octeon.h>
+#include <asm/octeon/cvmx-sata-defs.h>
+
+int ahci_octeon_config(struct platform_device *pdev)
+{
+	cvmx_sata_uctl_shim_cfg_t shim_cfg;
+
+	/* set-up endian mode */
+	shim_cfg.u64 = cvmx_read_csr(CVMX_SATA_UCTL_SHIM_CFG);
+#ifdef __BIG_ENDIAN
+	shim_cfg.s.dma_endian_mode = 1;
+	shim_cfg.s.csr_endian_mode = 1;
+#else
+	shim_cfg.s.dma_endian_mode = 0;
+	shim_cfg.s.csr_endian_mode = 0;
+#endif
+	cvmx_write_csr(CVMX_SATA_UCTL_SHIM_CFG, shim_cfg.u64);
+}
