8c7f0822a9c9f3dce284e04e3591908e4aa79f62
David Daney <david.daney@cavium.com>
2015-02-13 12:57:20 +0530

    netdev: octeon: Move and update OCTEON network drivers from staging.
    netdev: octeon-ethernet: Enable SKB recycling of transmitted packets.
    
    New module parameter "octeon_recycle_tx" turns it on and off.
    
    net: Export skb_release_head_state.
    
    This function will be needed by octeon-ethernet driver
    
    Signed-off-by: David Daney <david.daney@cavium.com>
    Signed-off-by: Abhishek Paliwal <abhishek.paliwal@aricent.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -573,7 +573,7 @@ static void kfree_skbmem(struct sk_buff *skb)
 	}
 }
 
-static void skb_release_head_state(struct sk_buff *skb)
+void skb_release_head_state(struct sk_buff *skb)
 {
 	skb_dst_drop(skb);
 #ifdef CONFIG_XFRM
@@ -597,6 +597,7 @@ static void skb_release_head_state(struct sk_buff *skb)
 #endif
 #endif
 }
+EXPORT_SYMBOL(skb_release_head_state);
 
 /* Free everything but the sk_buff shell. */
 static void skb_release_all(struct sk_buff *skb)
